92481fb86590507f672e6ff8091d5c63f7f99ba8
patmuchiri <pmuchirig@gmail.com>
2024-09-23 12:26:24 +0300

    added forgot password feature

--- billing/urls.py
+++ billing/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 from .views import landing, login_view, register_user, register_client, admin_dashboard, view_clients, edit_client, delete_client
 from django.contrib.auth.views import LogoutView
@@ -15,4 +16,10 @@ urlpatterns = [
     path('view_clients/', view_clients, name='view_clients'),
     path('view_users/', views.view_users, name='view_users'),
     path('logout/', LogoutView.as_view(next_page='landing'), name='logout'),
+
+    # Password reset URLs using Django's built-in views
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
